0a96b8f3cfc6d16452e215234cbc8312b06be408
Ferdous Ahmed <tanin@Ferdouss-MacBook-Pro.local>
2015-01-01 13:11:02 -0500

    Initial Commit

---
+++ Rain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rain
+//
+//  Created by Ferdous Ahmed on 1/1/15.
+//  Copyright (c) 2015 Ferdous Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
